eecdb732907ab945d0fb0a424a5ceccabaf2e2f2
TheProtectedProgrammer <jordanomiller99@gmail.com>
2018-04-16 23:06:48 +0100

    Added Basic Player Movement
    Basic player movement along with some simple textures were added.
    
    - The Protected Programmer

---
+++ Game/Assets/Scripts/User Control/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+    public float playerRotateSpeed;
+    public float cameraZoom;
+    [Header("Required Game Objects")]
+    public Camera playerCamera;
+    [Header("Debug Area")]
+    public int buildType;
+
+	void Start () {
+        playerCamera.transform.position = new Vector3(transform.position.x, cameraZoom, transform.position.z);
+	}
+	
+	void Update () {
+		if(buildType == 1) {
+            // PC or Mac
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * Time.deltaTime * playerSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position -= transform.forward * Time.deltaTime * playerSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, Time.deltaTime * -playerRotateSpeed, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, Time.deltaTime * playerRotateSpeed, 0);
+            }
+        }
+
+        if(buildType == 2)
+        {
+            // Controller
+        }
+	}
+}
